562cd26dbbbe8d6196ac3d67c8ec9fcbcb76be68
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-09 23:46:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[?!.]/).reject {|string| string.nil? || 
+    new_array = self.split(/[?!.]/).reject {|string| string.nil? || string == ""}
     new_array.length 
   end
 end
